27b72ae854c2309e9b7d12d9005084df1cef71be
Kia <68226341+Yeon09-a@users.noreply.github.com>
2024-01-08 23:59:36 +0900

    [Bronze II] Title: 단어의 개수, Time: 40 ms, Memory: 4916 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -5,27 +5,22 @@ using namespace std;
 int main() {
 	
 	string a;
-	long count = 0;
+	int count = 1;
 	getline(cin, a);
 	
-	if (a.empty() || !a.compare(" ")) {
-		cout << count;
+	for (int i = 0; i < a.length(); i++) {
+		if (a[i] == char(' ')) {
+			count++;
+		}
 	}
-	else {
-		if (a.front() == char(' ')) {
-			a.erase(0, 1);
-		}
 
-		if (a.back() == char(' ')) {
-			a.pop_back();
-		}
-
-		for (long i = 0; i < a.length(); i++) {
-			if (a[i] == char(' ')) {
-				count++;
-			}
-		}
-
-		cout << count + 1;
+	if (a.front() == ' ') {
+		count--;
 	}
+
+	if (a.back() == ' ') {
+		count--;
+	}
+
+	cout << count;
 }
