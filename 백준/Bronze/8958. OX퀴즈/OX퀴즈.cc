ec711c565fd373c15b9a9d28ee620f27cea0c81a
Kia <68226341+Yeon09-a@users.noreply.github.com>
2024-01-12 19:26:41 +0900

    [Bronze II] Title: OX퀴즈, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	
+	int c;
+
+	string a[100];
+	int s[100];
+	int add = 0;
+
+	cin >> c;
+	cin.ignore();
+
+	for (int i = 0; i < c; i++) {
+		getline(cin, a[i]);
+		
+		for (int k = 0; k < a[i].length(); k++) {
+			if (a[i][k] == 'O') {
+				s[i] += add + 1;
+				add++;
+			}
+			else {
+				add = 0;
+			}
+		}
+		add = 0;
+	}
+
+	for (int k = 0; k < c; k++) {
+		cout << s[k]<< endl;
+	}
+
+	return 0;
+}
