b47d4558c645d6cbe99a78ee0409ecdec88258a0
Kia <68226341+Yeon09-a@users.noreply.github.com>
2024-01-12 17:35:14 +0900

    [Bronze II] Title: 음계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.cc
+++ 백준/Bronze/2920. 음계/음계.cc
@@ -3,35 +3,29 @@
 int main() {
 	
 	int i[8];
+	int result = 0;
 
 	for (int k = 0; k < 8; k++) {
 		scanf("%d", &i[k]);
 	}
 	
 	for (int j = 0; j < 7; j++) {
-		if (i[j] == j + 1) {
-			if (!(i[j + 1] == i[j] + 1)) {
-				printf("mixed");
-				return 0;
-			}
+		if (i[j + 1] == i[j] + 1) {
+			result++;
 		}
-		else if (i[j] + j == 8) {
-			if (!(i[j + 1] == i[j] - 1)) {
-				printf("mixed");
-				return 0;
-			}
-		}
-		else {
-			printf("mixed");
-			return 0;
+		else if (i[j + 1] == i[j] - 1) {
+			result--;
 		}
 	}
 
-	if (i[0] == 1) {
+	if (result == 7) {
 		printf("ascending");
 	}
-	else {
+	else if (result == -7) {
 		printf("descending");
+	}
+	else {
+		printf("mixed");
 	}
 
 	return 0;
