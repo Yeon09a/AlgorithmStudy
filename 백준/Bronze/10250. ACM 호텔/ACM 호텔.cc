e389ba97a48ef4eb8de053ec48dde8d39717f010
Kia <68226341+Yeon09-a@users.noreply.github.com>
2024-01-13 15:06:54 +0900

    [Bronze III] Title: ACM 호텔, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -4,18 +4,15 @@ int main() {
 	
 	int c;
 	int h, w, n;
-	int r[1000];
+	int r;
 
 	scanf("%d", &c);
 
 	for (int i = 0; i < c; i++) {
 		scanf("%d %d %d", &h, &w, &n);
 
-		r[i] = (n % h == 0 ? n / h : (n / h) + 1) + ((n % h == 0 ? h : n % h) * 100);
-	}
-
-	for (int i = 0; i < c; i++ ) {
-		printf("%d\n", r[i]);
+		r = (n % h == 0 ? n / h : (n / h) + 1) + ((n % h == 0 ? h : n % h) * 100);
+		printf("%d\n", r);
 	}
 
 	return 0;
