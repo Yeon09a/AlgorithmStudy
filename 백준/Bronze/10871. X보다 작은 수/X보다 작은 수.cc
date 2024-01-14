9f666763a2ffd89131cda601590cbda77fbc0351
Kia <68226341+Yeon09-a@users.noreply.github.com>
2024-01-14 14:01:34 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -1,23 +1,16 @@
 #include <stdio.h>
-#include <vector>
-using namespace std;
 
 int main() {
 	
 	int n, x, m;
-	vector<int> result = {};
 
 	scanf("%d %d", &n, &x);
 
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &m);
 		if (m < x) {
-			result.push_back(m);
+			printf("%d ", m);
 		}
-	}
-
-	for (int j = 0; j < result.size(); j++) {
-		printf("%d ", result.at(j));
 	}
 
 	return 0;
