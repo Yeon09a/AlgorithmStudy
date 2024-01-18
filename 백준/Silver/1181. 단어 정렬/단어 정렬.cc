433ad3d69b61b46a0f092e4f66728bd429b8998a
Kia <68226341+Yeon09-a@users.noreply.github.com>
2024-01-18 17:34:59 +0900

    [Silver V] Title: 단어 정렬, Time: 904 ms, Memory: 4108 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int n;
+string words[20000];
+
+bool cmp(string a, string b) {
+	
+	if (a.length() < b.length()) {
+		return true;
+	}
+	else if (a.length() == b.length()) {
+		if (a < b) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+int main() {
+	
+
+	cin >> n;
+	cin.ignore();
+
+	for (int k = 0; k < n; k++) {
+		getline(cin, words[k]);
+	}
+
+	sort(words, words + n, cmp);
+
+	cout << words[0] << endl;
+	for (int j = 1; j < n; j++) {
+		
+		if (words[j].compare(words[j - 1])) {
+			cout << words[j] << endl;
+		}
+	}
+
+	return 0;
+}
