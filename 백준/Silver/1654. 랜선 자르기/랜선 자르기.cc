03f32ceebb4bc7763d6614e8fd908de3e5b0a207
Kia <68226341+Yeon09-a@users.noreply.github.com>
2024-01-22 22:16:43 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,59 +1,40 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
-unsigned int ans;
-unsigned int N,K;
-unsigned int list[10000];
+int main() {
 
-int main()
-{
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(NULL);
+	int k, n;
+	unsigned int from = 1, to = 0, mid;
+	unsigned int result = 0;
+	unsigned int lan[10000];
 
-	cin >> K >> N;
-    
-	unsigned int maxi = 0;
-    
-	for (int i = 0; i < K; i++)
-	{
-		cin >> list[i];
-		maxi = max(maxi, list[i]);
+	cin >> k >> n;
+
+	for (int i = 0; i < k; i++) {
+		cin >> lan[i];
+		to = max(to, lan[i]);
 	}
 
-	unsigned int left = 1, right = maxi, mid;
-	
-	while (left <= right)
-	{
-		// mid 연산
-		mid = (left + right) / 2;
-        
-		// 몫의 합을 저장하는 변수
-		unsigned int now = 0; 
-        
-		for (int i = 0; i < K; i++)
-		{
-			//mid로 나눈 몫을 저장
-			now += list[i] / mid;
+	while (from <= to) {
+		int sum = 0;
+
+		mid = (from + to) / 2;
+
+		for (int j = 0; j < k; j++) {
+			sum += lan[j] / mid;
 		}
 
-		if (now >= N)
-		{
-			// 현재 mid로 나눈 값이 N보다 크거나 같다면,
-			// left를 움직여 길이가 더 긴 경우는 가능한지 검사
-			left = mid + 1;
-            
-			// N개를 만들 수 있을 때, 답을 더 큰 값으로 계속 갱신
-			ans = max(ans, mid);
+		if (sum >= n) {
+			from = mid + 1;
+			result = max(result, mid);
 		}
-		else
-		{
-			// 현재 mid로 나눈 값이 N보다 작다면,
-			// right 움직여 길이가 더 짧은 경우는 가능한지 검사
-			right = mid - 1;
+		else {
+			to = mid - 1;
 		}
 	}
-	
-	cout << ans << '\n';
+
+	cout << result;
+
+	return 0;
 }
