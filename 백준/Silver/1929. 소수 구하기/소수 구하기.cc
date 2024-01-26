609efb01733db724790e85d80112e11df8f3a30f
Kia <68226341+Yeon09-a@users.noreply.github.com>
2024-01-26 15:13:29 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 1968 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -4,27 +4,23 @@
 int main() {
 
 	int m, n;
-	int arr[1000001];
+	bool arr[1000001] = {true, true, false, };
 
 	scanf("%d %d", &m, &n);
 
-	for (int i = 2; i <= n; i++) {
-		arr[i] = i;
-	}
-
 	for (int j = 2; j * j <= n; j++) {
-		if (arr[j] == 0) {
+		if (arr[j]) {
 			continue;
 		}
 		
 		for (int k = j * j; k <= n; k += j) {
-			arr[k] = 0;
+			arr[k] = true;
 		}
 	}
 
 	for (int p = m; p <= n; p++) {
-		if (arr[p] != 0) {
-			printf("%d\n", arr[p]);
+		if (!arr[p]) {
+			printf("%d\n", p);
 		}
 	}
 
