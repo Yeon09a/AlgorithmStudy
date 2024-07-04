3edad1b80d6163a5c2ade4262429b1d059df36d0
Kim Yeona <68226341+Yeon09a@users.noreply.github.com>
2024-07-04 14:55:48 +0900

    Create 요세푸스 문제 0 not queue ver.cc

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0 not queue ver.cc
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int n, k;
+	bool bnums[1001] = { false, };
+	int nums[1001];
+
+	scanf("%d %d", &n, &k);
+	
+	int count = n;
+	int bindex = 0;
+	int index = 0;
+
+	while (count > 0) {
+		int i = k;
+
+		while (i > 0) {
+			bindex++;
+
+			if (bindex > n) {
+				bindex = 1;
+			}
+			
+			if (!bnums[bindex]) {
+				i--;
+			}
+		}
+
+		bnums[bindex] = true;
+		nums[index] = bindex;
+		index++;
+		count--;
+	}
+
+	printf("<");
+	for (int i = 0; i < n - 1; i++) {
+		printf("%d, ", nums[i]);
+	}
+	printf("%d>", nums[n - 1]);
+}
+
+// 메모리 1116 KB
+// 시간 4 ms
